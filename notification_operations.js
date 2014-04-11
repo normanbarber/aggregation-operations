1123cabbc89c2a992a4b2cd1ba9993f2ccdcee35
normanbarber <normanebarber@gmail.com>
2014-04-11 16:14:51 -0400

    adding an operation for counting total notifications

--- notification_operations.js
+++ notification_operations.js
@@ -7,10 +7,25 @@
 // outputting the users fname field and the total for each user   - using match to exclude some name
 cls
 db.contacts.aggregate([
-{$match : { fname : { $nin : ["joe", "joeadmin", "Bholbrooks", "Commandalkon", "Lmarthe", "Bzoelle", "cstuart"] }}},
+{$match : { fname : { $nin : ["names", "to", "exclude", "go", "here"] }}},
 {$unwind: "$registrations" },
 { $group: { _id: "$fname", count: { $sum: 1 } } },
 {$project : { "name" : "$_id", "_id" : 0, "count" : 1 }}
+])
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// unwinds the registrations array and counts the total notifications for all users
+// excludes all users with fname = names listed in the $nin array
+cls
+db.contacts.aggregate([
+{ $match: { fname : { $nin : ["names", "to", "exclude", "go", "here"] }}},
+{ $unwind: "$registrations" },
+{ 
+$group: { _id: "$registration", count: { $sum: 1 } }
+},
+{$project : { "_id" : 0, count : 1 }}
 ])
 
 
